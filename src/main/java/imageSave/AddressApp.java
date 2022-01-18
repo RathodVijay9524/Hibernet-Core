dc8f1ff24d6d80c6e1814965ac6d1e89f9428f55
Vijay_9524 <omvijay44>
2022-01-18 00:40:40 -0800

    Loded image in database changes

--- src/main/java/imageSave/AddressApp.java
+++ src/main/java/imageSave/AddressApp.java
@@ -4,17 +4,24 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Date;
 
-public class AddressApp {
+public class AddressApp  {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Address ad=new Address();
         ad.setCity("pune");
         ad.setStreet("Handewadi road");
         ad.setOpen(true);
         ad.setAddedDate(new Date());
         ad.setX(1234.234);
+
+        FileInputStream fis=new FileInputStream("C:\\Users\\VR\\IdeaProjects\\HibernetCore\\src\\main\\resources\\vijay.png");
+        byte[] data=new byte[fis.available()];
+        fis.read(data);
+        ad.setImage(data);
 
         Configuration cfg=new Configuration();
         cfg.configure("hibernate.cfg.xml");
