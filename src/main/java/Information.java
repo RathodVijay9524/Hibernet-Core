b453f2aa70a3f3ef45c8ae42e1b282f8969f0b71
Vijay_9524 <omvijay44>
2022-01-24 23:10:23 -0800

    changes

---
+++ src/main/java/Information.java
@@ -0,0 +1,78 @@
+public class Information {
+
+
+    public static void main(String[] args) {
+
+     /*   Configuration
+                SessionFactory
+        Session
+        @Entity
+        @Table(name = "customer")
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "id")
+        @OneToOne(mappedBy = "answer")
+        @ManyToOne
+        @JoinColumn(name="Q_id")
+        @OneToMany(mappedBy = "person", cascade = CascadeType.ALL)
+
+
+
+                DispatcherServlet
+
+        InternalResourceViewResolver
+                WebApplicationInitializer
+        WebMvcConfigurerAdapter
+                AbstractAnnotationConfigDispatcherServletInitializer
+        MessageSource
+                LocaleResolver
+        addInterceptors
+                addResourceHandlers
+
+
+
+        LocalEntityManagerFactoryBean
+                JpaTransactionManager
+
+        Environment
+                LocalSessionFactoryBean
+        DataSource
+                Properties
+        HibernateTransactionManager
+                UrlBasedViewResolver
+
+        @Configuration
+        @ComponentScan("com.sprhib")
+        @EnableWebMvc
+        @EnableTransactionManagement
+        @PropertySource("classpath:application.properties")
+        public class WebAppConfig
+
+        @Resource
+        @Bean
+        @Repository
+        @Autowired
+
+        @Service
+        @Transactional
+
+        @Controller
+        @RestController
+        @RequestMapping
+        @GetMapping
+        @PutMapping
+        @ModelAttribute
+        @PathVariable
+
+
+
+                database.driver = com.mysql.jdbc.Driver
+        database.url = jdbc:mysql://localhost:3306/concretepage
+        database.root = root
+        database.password =
+
+                hibernate.dialect = org.hibernate.dialect.MySQLDialect
+        hibernate.hbm2ddl.auto = update
+        hibernate.show_sql = true*/
+    }
+}
